b4fb12f69f1f900c1770a02176cf3afc9cb20233
GudjonGeir <gudjongeir@gmail.com>
2014-10-17 18:01:17 +0000

    Negative numbers not allowed, two tests

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,25 +1,39 @@
 package is.ru.stringcalculator;
 
+import java.util.*;
+
 public class Calculator {
 
-	public static int add(String text){
+	public static int add(String text) throws NegativeNumberException
+	{
+		String delimiter = ",";
+
+		if (text.length() > 5 && text.substring(0, 2).equals("//")) {
+			delimiter = text.substring(2, 3);
+			text = text.substring(4, text.length());
+		}
+
 		text = removeNewLines(text);
+
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
+		else if(text.contains(delimiter)){
+			String[] numbers = splitNumbers(text, delimiter);
+			checkNeg(numbers);
+			
+			return sum(numbers);
 		}
 		else
-			return 1;
+			return toInt(text);
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	private static String[] splitNumbers(String numbers, String delimiter){
+	    return numbers.split(delimiter);
 	}
       
     private static int sum(String[] numbers){
@@ -33,5 +47,33 @@ public class Calculator {
     private static String removeNewLines(String text){
     	return text.replace("\n", ",");
     }
+
+    private static void checkNeg(String[] numbers) throws NegativeNumberException
+    {
+    	String message = "Negatives not allowed: ";
+    	boolean negTrue = false;
+		for(int i = 0; i < numbers.length; i++)
+		{
+			if(toInt(numbers[i]) < 0)
+			{
+			if (!negTrue) message += numbers[i];
+			else message += "," + numbers[i];
+			negTrue = true;
+			}
+		}
+		if (negTrue) throw new NegativeNumberException(message);
+    }
+
+
+
+    public static class  NegativeNumberException extends Exception
+	{
+
+
+		public NegativeNumberException(String message)
+		{
+	 		super(message);
+		}
+ 	}
 
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package is.ru.stringcalculator;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import is.ru.stringcalculator.Calculator.NegativeNumberException;
+
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -10,28 +12,65 @@ public class CalculatorTest {
     }
 
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() throws NegativeNumberException {
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber() throws NegativeNumberException {
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
-	public void testTwoNumbers() {
+	public void testOneNumber2() throws NegativeNumberException {
+		assertEquals(2, Calculator.add("2"));
+	}
+
+	@Test
+	public void testTwoNumbers() throws NegativeNumberException {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
+    public void testMultipleNumbers() throws NegativeNumberException {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
     @Test
-    public void testNewLines(){
+    public void testNewLines() throws NegativeNumberException {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
+
+    @Test
+    public void testDelimiter() throws NegativeNumberException {
+    	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testNegative1() throws NegativeNumberException {
+    	try 
+    	{
+    		Calculator.add("-1,2");
+    		fail( "Expected exception not thrown" );
+		} 
+		catch (NegativeNumberException ex) 
+		{
+			assertEquals("Negatives not allowed: -1", ex.getMessage());
+		}
+
+    }
+
+    @Test
+    public void testNegative2() throws NegativeNumberException {
+    	try 
+    	{
+    		Calculator.add("2,-4,3,-5");
+    		fail( "Expected exception not thrown" );
+		} 
+		catch (NegativeNumberException ex) 
+		{
+			assertEquals("Negatives not allowed: -4,-5", ex.getMessage());
+		}
+	}
 
 }
